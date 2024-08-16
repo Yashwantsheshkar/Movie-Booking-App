0b177e20465d18052f8eaef77085201805393fc8
Yashwantsheshkar <yashwantsheshkar@gmail.com>
2024-08-17 02:30:38 +0530

    Initial Commit

---
+++ Movie Booking App/Movie_Booking_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_Booking_AppApp.swift
+//  Movie Booking App
+//
+//  Created by Yashwant Sheshkar on 17/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Movie_Booking_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
